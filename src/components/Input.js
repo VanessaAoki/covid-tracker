4c607d96af72500bad9bccdcf0958f13d60794c8
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-20 23:47:05 -0300

    Add input component

---
+++ src/components/Input.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import DataCard from './AllRegionsCard';
+
+const Input = (props) => {
+  const { data } = props;
+  let searchResult = '';
+  const [state, setState] = React.useState({
+    search: '',
+  });
+
+  const handleChange = (e) => {
+    setState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  if (state) {
+    const inputValue = state.search.toLowerCase();
+    searchResult = data.filter((name) => name.id.includes(inputValue))
+      .map((info) => (info));
+  }
+
+  return (
+    <>
+      <form>
+        <input type="text" value={state.value} name="search" placeholder="Search by state name" onChange={handleChange} id="searchInput" />
+      </form>
+      <DataCard data={searchResult} />
+    </>
+  );
+};
+
+export default Input;
