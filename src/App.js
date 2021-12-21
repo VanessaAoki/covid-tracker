32034d8648679f320342bc183583bdba7f201ff1
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-20 23:34:17 -0300

    Delete boilerplate and add basic files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import MainPage from './components/Main';
+import SecondPage from './components/Details';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Router>
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/main" />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/details" element={<SecondPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
