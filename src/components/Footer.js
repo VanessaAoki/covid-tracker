a02eaf44ea47f6232f75d3b0a1b8bf099fc7414c
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-20 23:48:05 -0300

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    **Data gathered from
+    {' '}
+    <a href="https://covid19tracking.narrativa.com/index_en.html">Narrativa API</a>
+  </footer>
+);
+
+export default Footer;
